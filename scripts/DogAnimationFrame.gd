29862b75909ea361d5f9880234d98d8a69f4e4c0
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2023-10-31 02:35:06 +0000

    gh pages branch

--- scripts/DogAnimationFrame.gd
+++
@@ -1,5 +0,0 @@
-class_name DogAnimationFrame
-extends Resource
-
-@export var rotation: float = 0.0
-@export var position: Vector2 = Vector2.ZERO
--- scripts/server.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-func _ready():
-	DisplayServer.window_set_size(Vector2(20, 20))
