68494bbfb61c9cb6d7f00f9132d8a93fb6f967ba
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2023-11-02 17:17:46 +0000

    Esc while paused closes submenu

--- scripts/pause.gd
+++ scripts/pause.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var submenu = null
+
 func _process(_delta):
 	if Global.pause_enable == false:
 		return
@@ -7,19 +9,25 @@ func _process(_delta):
 		get_tree().paused = true
 		visible = true
 	elif Input.is_action_just_pressed("pause")  and get_tree().paused and visible:
-		get_tree().paused = false
-		visible = false
-		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+		if is_instance_valid(submenu):
+			submenu.queue_free()
+		else:
+			get_tree().paused = false
+			visible = false
+			Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	if get_tree().paused and visible:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _on_resume_pressed():
 	get_tree().paused = false
 	visible = false
-	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN	
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 func _on_edit_palette_pressed():
-	add_child(preload("res://scenes/set_palette.tscn").instantiate())
+	submenu = preload("res://scenes/set_palette.tscn").instantiate()
+	add_child(submenu)
 
 func _on_leave_server_pressed():
-	multiplayer.peer.close()
+	get_tree().paused = false
+	visible = false
+	multiplayer.multiplayer_peer.close()
