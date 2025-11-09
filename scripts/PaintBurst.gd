aeec6d1132359bf6b5696827ce9dc9705aa02b88
Your Name <44114870+JdavisBro@users.noreply.github.com>
2025-11-09 09:49:45 +0000

    paintburst perf improvement

--- scripts/PaintBurst.gd
+++ scripts/PaintBurst.gd
@@ -3,17 +3,17 @@ class_name PaintBurst
 
 var paint
 
-var queue = []
-var dist = 0.0
-var speed = 0.0
-var accel = 0.0
-var done = BitMap.new()
-var farthest_dist = 0
+var queue: Array[Vector2i] = []
+var dist := 0.0
+var speed := 0.0
+var accel := 0.0
+var done := BitMap.new()
+var farthest_dist := 0.0
 
-var color = 0
-var color_collide = -1 # -1 = no collide, 0+ = collide with all but color index (0 empty)
+var color := 0
+var color_collide := -1 # -1 = no collide, 0+ = collide with all but color index (0 empty)
 
-const CHECKED_CAP = 60
+const CHECKED_CAP := 60
 
 func _init(n_paint, n_color, n_color_collide=-1):
 	paint = n_paint
@@ -26,40 +26,48 @@ func stop():
 func _ready():
 	add_to_group("paintbursts")
 	process_mode = PROCESS_MODE_PAUSABLE
-	queue.append(position.round())
+	queue.append(Vector2i(position.round()))
 	done.create(Vector2i(paint.size))
 
-func _process(delta):
+func _process(delta: float):
 	dist += speed*60*delta
 	speed += accel*delta
 	if speed <= 0:
 		accel = 0.02*60 # values from original game times framerate for delta
-	var checked = 0
-	var end_time = Time.get_ticks_msec() + 1 # this is in the base game. time cap of 2ms
-	if len(get_tree().get_nodes_in_group("paintbursts")) <= 5:
-		end_time += 2
+	var checked := 0
+	#var end_time := Time.get_ticks_msec() + 1 # this is in the base game. time cap of 2ms
+	#if len(get_tree().get_nodes_in_group("paintbursts")) <= 5:
+		#end_time += 2
+	var end_time: float = Time.get_ticks_msec() + (3 - min(10, len(get_tree().get_nodes_in_group("paintbursts"))) / 5)
 	while queue and checked < CHECKED_CAP and Time.get_ticks_msec() < end_time:
-		var pos = queue.pop_front()
-		if color_collide > -1:
-			if paint.paint.at(pos.x, pos.y) != color_collide:
-				continue
-		var cur_dist = position.distance_to(pos)
+		var pos: Vector2i = queue.pop_front()
+		if color_collide > -1 and paint.paint.at(pos.x, pos.y) != color_collide:
+			continue
+		var cur_dist := position.distance_to(pos)
 		farthest_dist = max(cur_dist, farthest_dist)
 		if cur_dist > dist:
 			queue.append(pos)
 			break
 		PaintUtil.update_pos(paint, color, pos)
-		var diff = Vector2(1, 1) # pos/neg is random in game but it only changes queue order so doesn't matter that much
-		for newpos in [pos+Vector2(diff.x, 0), pos+Vector2(0, diff.y), pos-Vector2(diff.x, 0), pos-Vector2(0, diff.y)]:
-			add_newpos(newpos)
+		#var diff = Vector2(1, 1) # pos/neg is random in game but it only changes queue order so doesn't matter that much
+		add_newpos(Vector2i(pos.x + 1, pos.y))
+		add_newpos(Vector2i(pos.x, pos.y + 1))
+		add_newpos(Vector2i(pos.x - 1, pos.y))
+		add_newpos(Vector2i(pos.x, pos.y - 1))
+		#for newpos in [pos+Vector2(diff.x, 0), pos+Vector2(0, diff.y), pos-Vector2(diff.x, 0), pos-Vector2(0, diff.y)]:
+			#add_newpos(newpos)
 		if cur_dist < farthest_dist and (color_collide == -1 or Settings.fill_bucket_do_corners):
 			#diff = Vector2(1, 1) # this is rerandomised here but again it doesn't matter much
-			for newpos in [pos+Vector2(diff.x, diff.y), pos+Vector2(-diff.x, diff.y), pos+Vector2(diff.x, -diff.y), pos+Vector2(-diff.x, -diff.y)]:
-				add_newpos(newpos)
+			add_newpos(Vector2i(pos.x + 1, pos.y + 1))
+			add_newpos(Vector2i(pos.x - 1, pos.y + 1))
+			add_newpos(Vector2i(pos.x + 1, pos.y - 1))
+			add_newpos(Vector2i(pos.x - 1, pos.y - 1))
+			#for newpos in [pos+Vector2(diff.x, diff.y), pos+Vector2(-diff.x, diff.y), pos+Vector2(diff.x, -diff.y), pos+Vector2(-diff.x, -diff.y)]:
+				#add_newpos(newpos)
 	if not queue:
 		queue_free()
 
-func add_newpos(newpos):
+func add_newpos(newpos: Vector2i):
 	if newpos.x >= paint.size.x or newpos.x < 0 or newpos.y >= paint.size.y or newpos.y < 0:
 		return
 	if not done.get_bitv(newpos):
